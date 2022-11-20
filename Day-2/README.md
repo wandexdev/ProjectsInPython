53385a024fef116f58a4e1c8ae42b816dcc4568d
wandexdev <tiwande04@gmail.com>
2022-11-20 16:12:34 +0100

    Task 1 day 2

--- Day-2/README.md
+++ Day-2/README.md
@@ -1,3 +1,8 @@
 # Day 2
 
 ## Task 1
+ * Subscrpting strings via `[]`
+ * Idenitfy data type with `type()`
+ * Convert data types i.e `int()` to `str()` to `float() and vice versa
+ * Addition
+
---
+++ Day-2/task1of2.py
@@ -0,0 +1,11 @@
+user_no = input("give me two numbers ")
+#print(type(user_no))
+
+num_one = user_no[0]
+num_two = user_no[1]
+
+new_num_one = int(num_one)
+new_num_two = int(num_two)
+
+print(new_num_one + new_num_two)
+
