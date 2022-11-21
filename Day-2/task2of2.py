e05780fc615d5c539fc3a33cd3b3b28ceab4b2cf
wandexdev <tiwande04@gmail.com>
2022-11-21 14:03:27 +0100

    convert to whole num

--- Day-2/task2of2.py
+++ Day-2/task2of2.py
@@ -4,4 +4,9 @@ weight = input("give me your weight in kg ")
 height = input("give me your height in m ")
 
 BMI = float(weight) / float(height) ** 2
+print(type(BMI))
 print(BMI)
+
+Integer_BMI = int(BMI)
+print(type(Integer_BMI))
+print(Integer_BMI)
