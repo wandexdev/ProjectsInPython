6d9791d0a2a469e12b87734f8d0d5af918ba2261
wandexdev <tiwande04@gmail.com>
2023-01-03 12:27:06 +0100

    Task 1 completed

--- Day-3/README.md
+++ Day-3/README.md
@@ -1,3 +1,10 @@
 # Day 3
 
 ## Task 1
+<img src="task1of3.png"
+     alt="Ouput" />
+ * Used Conditional statements syntax: if/else (either a or b)
+  * if whatever:
+  	do this
+    else:
+    	do this
---
+++ Day-3/task1of3.py
@@ -0,0 +1,7 @@
+print("==== WANDE's DIGITAL ROLLERCOASTER RIDE PERMIT ====")
+print ()
+Height = int(input("what is your height in cm? "))
+if Height > 20:
+    print("You've been permitted entry!")
+else:
+    print("You've been denied entry")
