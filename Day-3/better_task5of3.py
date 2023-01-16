f5dd7bbadc26974d431419edb594c0416f4f0591
wandexdev <tiwande04@gmail.com>
2023-01-16 15:11:42 +0100

    fewer code: updated

--- Day-3/better_task5of3.py
+++ Day-3/better_task5of3.py
@@ -1,1 +1,23 @@
-size = input("what size of pizza do you prefer? small, medium or Large? ")pepperoni = input()
+pizzaSize = input("What size of pizza do you prefer? small, medium or Large ")
+pepperoni = input("Do you want some pepperoni? ")
+xtraCheese = input("Would you like some extra cheese? ")
+
+fee = 0
+
+if pizzaSize == "small":
+    fee += 15
+elif pizzaSize == "medium":
+    fee += 20
+else:
+    fee += 25
+
+if pepperoni == "yes":
+    if pizzaSize == "small":
+        fee += 2
+    else:
+        fee += 3
+
+if xtraCheese == "yes":
+    fee += 1
+
+print(f"Your total bill is ${fee}")
