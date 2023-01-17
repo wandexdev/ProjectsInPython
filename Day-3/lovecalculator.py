00cac7fd74e74b547f757f0ad03e8243b6dce926
wandexdev <tiwande04@gmail.com>
2023-01-17 23:26:55 +0100

    updated: lovecalculator

--- Day-3/lovecalculator.py
+++ Day-3/lovecalculator.py
@@ -21,5 +21,6 @@ score = str(true) + str(love)
 if score < 10 or score > 90:
     print(f"your score is {score}, you go together like coke and mentos")
 elif score >= 40 and score <= 50:
-    print (f"youre score is {}, you are alright together")    
-       
+    print (f"your score is {score}, you are alright together")    
+else:
+    print(f"youre score is {score}")     
